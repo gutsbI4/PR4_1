5458e8eff9e919b5d23320014fa289a9b7fc1f91
miron256 <mironlevichev86@gmail.com>
2023-03-06 18:19:10 +0300

    zadan1-zadan2 done

--- UserControls/UserControlCheckBoxForPr4.axaml.cs
+++ UserControls/UserControlCheckBoxForPr4.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media.Imaging;
+using Avalonia.Styling;
 using System;
 
 namespace PR4_1.UserControls
@@ -12,9 +13,29 @@ namespace PR4_1.UserControls
             InitializeComponent();
         }
 
-        public void Click_Image(object sender, PointerPressedEventArgs e)
+        public void Border_Click(object sender, PointerPressedEventArgs e)
         {
-            
+            Border border = (Border)sender;
+            if(border.Name == "sadBorder")
+            {
+                border.Opacity = 0.7;
+                happyBorder.Opacity = 0.1;
+            }
+            else if(border.Name == "happyBorder")
+            {
+                border.Opacity = 0.7;
+                sadBorder.Opacity = 0.1;
+            }
+        }
+        public void Border_Enter(object sender, PointerEventArgs e)
+        {
+            Border border = (Border)sender;
+            if (border.Opacity != 0.7) border.Opacity = 0.3;
+        }
+        public void Border_Leave(object sender, PointerEventArgs e)
+        { 
+            Border border = (Border)sender;
+            if (border.Opacity != 0.7) border.Opacity = 0.1;
         }
     }
 }
